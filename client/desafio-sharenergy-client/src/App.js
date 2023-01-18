296bb95256b7b30fe360dea51f4753201a24f6e7
hikarocarvalho <hikarofcarvalho@gmail.com>
2023-01-18 00:31:10 -0300

    feat(routes): Imported routes to the app file, and created one context to modal calls

--- client/desafio-sharenergy-client/src/App.js
+++ client/desafio-sharenergy-client/src/App.js
@@ -1,9 +1,36 @@
+import { useState, useEffect, createContext } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import MenuRoutes from './Routes/MenuRoutes';
+import PageRoutes from './Routes/PageRoutes';
+import Modal from './Components/Modal/Modal';
 import './App.css';
 
+export const ModalContext = createContext(null);
+
 function App() {
+  const [appState, setAppState] = useState(
+    {
+      modal:{
+        view:false,
+        title:"",
+        component:<></>
+      }
+    }
+  );
+
+  useEffect(()=>{
+
+  },[]);
+
   return (
     <div className="App">
-     
+      <ModalContext.Provider value={{appState,setAppState}}>
+        <BrowserRouter>
+          <MenuRoutes />
+          <PageRoutes />
+        </BrowserRouter>
+        <Modal />
+      </ModalContext.Provider>
     </div>
   );
 }
