7f4b891cac0bd3fe2d49f6a4c9ccaed6341d9f8b
hikarocarvalho <hikarofcarvalho@gmail.com>
2023-01-19 18:05:21 -0300

    feat(RandomDog): Create RandomDog structure

--- client/desafio-sharenergy-client/src/Pages/RandomDog/RandomDog.js
+++ client/desafio-sharenergy-client/src/Pages/RandomDog/RandomDog.js
@@ -1,8 +1,13 @@
-import './RandomDog.css';
+import Button from "../../Components/FormComponents/Button/Button";
+import "./RandomDog.css";
 
-export default function RandomDog(){
-    return (
-        <section className="random-dog">
-        </section>
-    );
-};
+export default function RandomDog() {
+  return (
+    <section className="random-dog page">
+      <div className="stats-container">
+        <i className="bi bi-card-image cat-image"></i>
+        <Button description={"Gerar Novo Cachorro"} />
+      </div>
+    </section>
+  );
+}
