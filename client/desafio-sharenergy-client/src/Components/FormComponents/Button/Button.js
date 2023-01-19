31d8958a634ed428dd0ee6a5ff048ed7824c9431
hikarocarvalho <hikarofcarvalho@gmail.com>
2023-01-19 18:01:01 -0300

    feat(Button): Add onClick props to Button component

--- client/desafio-sharenergy-client/src/Components/FormComponents/Button/Button.js
+++ client/desafio-sharenergy-client/src/Components/FormComponents/Button/Button.js
@@ -1,7 +1,26 @@
 import "./Button.css";
 
-export default function Button(props){
-    return(
-        <button className="btn btn-success btn-md">{props.description}</button>
-    );
+export default function Button(props) {
+  return props.onClick ? (
+    <button
+      className={
+        props.className
+          ? "btn btn-success btn-md " + props.className
+          : "btn btn-success btn-md"
+      }
+      onClick={props.onClick}
+    >
+      {props.description}
+    </button>
+  ) : (
+    <button
+      className={
+        props.className
+          ? "btn btn-success btn-md " + props.className
+          : "btn btn-success btn-md"
+      }
+    >
+      {props.description}
+    </button>
+  );
 }
