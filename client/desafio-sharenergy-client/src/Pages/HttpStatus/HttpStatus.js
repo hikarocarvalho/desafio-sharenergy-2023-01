76b7d0525a07d37e2b4bd3e12dcace3cce8b565a
hikarocarvalho <hikarofcarvalho@gmail.com>
2023-01-19 18:04:47 -0300

    feat(HttpStatus): Create HttpStatus structure

--- client/desafio-sharenergy-client/src/Pages/HttpStatus/HttpStatus.js
+++ client/desafio-sharenergy-client/src/Pages/HttpStatus/HttpStatus.js
@@ -1,8 +1,13 @@
-import './HttpStatus.css';
+import Select from "../../Components/FormComponents/Select/Select";
+import "./HttpStatus.css";
 
-export default function HttpStatus(){
-    return (
-        <section className="http-status">
-        </section>
-    );
-};
+export default function HttpStatus() {
+  return (
+    <section className="http-status page">
+      <div className="stats-container">
+        <i className="bi bi-card-image cat-image"></i>
+        <Select />
+      </div>
+    </section>
+  );
+}
