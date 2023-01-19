54124b4f163094b778fade2b7322a045cc703534
hikarocarvalho <hikarofcarvalho@gmail.com>
2023-01-19 18:03:39 -0300

    feat(User): add Search and List components to user page

--- client/desafio-sharenergy-client/src/Pages/User/User.js
+++ client/desafio-sharenergy-client/src/Pages/User/User.js
@@ -1,8 +1,12 @@
-import './User.css';
+import Search from "../../Components/Search/Search";
+import List from "../../Components/ListComponents/List/List";
+import "./User.css";
 
-export default function User(){
-    return (
-        <section className="user">
-        </section>
-    );
-};
+export default function User() {
+  return (
+    <section className="user page">
+      <Search />
+      <List />
+    </section>
+  );
+}
