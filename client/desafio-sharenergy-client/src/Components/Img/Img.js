06730fdd857b6dbd26a26f9ff40d325eb16d3394
hikarocarvalho <hikarofcarvalho@gmail.com>
2023-01-20 12:31:31 -0300

    feat(Img): verify if props src is not null

--- client/desafio-sharenergy-client/src/Components/Img/Img.js
+++ client/desafio-sharenergy-client/src/Components/Img/Img.js
@@ -4,10 +4,14 @@ export default function Img(props) {
   return (
     <>
       {props.src ? (
-        props.src.includes(".mp4") ? (
-          <iframe src={props.src} className="video" />
+        props.src !== "" ? (
+          props.src.includes(".mp4") ? (
+            <iframe src={props.src} className="video" title="video" />
+          ) : (
+            <img src={props.src} className="image" alt={props.alt} />
+          )
         ) : (
-          <img src={props.src} className="image" />
+          ""
         )
       ) : (
         ""
