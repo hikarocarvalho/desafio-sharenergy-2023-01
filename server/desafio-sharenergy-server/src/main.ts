f5625ccfb4638d3bb129d7ccbba509e91dfb0222
hikarocarvalho <hikarofcarvalho@gmail.com>
2023-01-19 23:55:23 -0300

    feat(main): create the swagger entry point description. configurate cors police and validation configurations

--- server/desafio-sharenergy-server/src/main.ts
+++ server/desafio-sharenergy-server/src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Sharenergy challenge 2023')
+    .setDescription('This is the Api to - the Sharenergy challenge 2023 ')
+    .setVersion('1.0')
+    .addTag('Sharenergy challenge 2023')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
